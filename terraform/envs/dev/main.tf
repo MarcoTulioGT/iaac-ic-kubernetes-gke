5d792b1bd61ba5f3ec94f30aa86433fbb9d4eb00
MarcoTulioGT <devopsgt@gmail.com>
2025-10-24 18:07:50 -0600

    add namespaces

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -11,3 +11,9 @@ module "gke_single_cluster" {
   cluster_name = var.cluster_name
 }
 
+module "gke_resources" {
+  source      = "../../modules/gke_resources"
+  project_id   = var.project_id
+  region       = var.region
+  cluster_name = var.cluster_name
+}
--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -40,27 +40,3 @@ resource "google_container_node_pool" "primary_nodes" {
   }
 }
 
-resource "kubernetes_namespace" "api_namespace" {
-  metadata {
-    name = "apis"
-  }
-}
-
-resource "kubernetes_namespace" "engines_namespace" {
-  metadata {
-    name = "engines"
-  }
-}
-
-resource "kubernetes_namespace" "portals_namespace" {
-  metadata {
-    name = "portals"
-  }
-}
-
-
-resource "kubernetes_namespace" "lakehouse_namespace" {
-  metadata {
-    name = "lakehouse"
-  }
-}
