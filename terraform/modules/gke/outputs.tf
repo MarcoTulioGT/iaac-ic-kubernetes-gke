5a1e628155174ce5a84b2a7f53c92cc719c58803
MarcoTulioGT <devopsgt@gmail.com>
2025-10-24 18:49:07 -0600

    add module

--- terraform/modules/gke/outputs.tf
+++ terraform/modules/gke/outputs.tf
@@ -1,7 +1,7 @@
 output "endpoint" {
-  value = google_container_cluster.primary.endpoint # O el nombre de tu recurso cluster
+  value = gke_cluster.primary.endpoint 
 }
 
 output "cluster_ca_certificate" {
-  value = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  value = gke_cluster.primary.master_auth[0].cluster_ca_certificate
 }  
