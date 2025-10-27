c16bfbdd94d712f4ddd713508c563fb308395797
MarcoTulioGT <devopsgt@gmail.com>
2025-10-26 19:32:10 -0600

    change structure3

--- terraform/gke-resources/providers.tf
+++ terraform/gke-resources/providers.tf
@@ -3,29 +3,3 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.terraform_remote_state.gke_state.outputs.cluster_ca_certificate)
   token                  = data.google_client_config.default.access_token
 }
-
-
-resource "kubernetes_namespace" "api_namespace" {
-  metadata {
-    name = "apis"
-  }
-}
-
-resource "kubernetes_namespace" "engines_namespace" {
-  metadata {
-    name = "engines"
-  }
-}
-
-resource "kubernetes_namespace" "portals_namespace" {
-  metadata {
-    name = "portals"
-  }
-}
-
-
-resource "kubernetes_namespace" "lakehouse_namespace" {
-  metadata {
-    name = "lakehouse"
-  }
-}
